771dba7b75f8736e667f65a945606b6b37cd4694
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-09-15 18:38:45 -0300

    Styles

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ if (edad >= 18) {
     mensajes.textContent= ` ✅ Tenes acceso ${nombre} porque tu edad es de ${edad}`
     clase = "ingresoExitoso"
 } else {
-    mensajes.textContent= ` 📛 No tenes acceso ${nombre}, tu edad es de ${edad} y tenes que ser mayor a 18 `
+    mensajes.textContent= ` 📛 No tenes acceso ${nombre}, tu edad es de ${edad} y tenes que ser mayor de 18 `
     clase = "ingresoFallido"
 }
 console.log(nombre, edad)
