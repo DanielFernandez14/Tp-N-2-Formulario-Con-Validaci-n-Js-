81a1b8c070567f5f00458b33238f009ea16c1f1c
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-09-15 17:31:59 -0300

    Form con validacion front

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hola ")
+const form = document.querySelector("#registroForm")
+const inputNombre = document.querySelector("#nombre")
+const inputEdad = document.querySelector("#edadd") 
+const mensajes = document.querySelector("#mensajeJS")
+
+function validacionDeFormulario(event) {
+    event.preventDefault();
+
+
+const nombre = inputNombre.value.trim()
+const edad = Number(inputEdad.value);
+
+if (edad >= 18) {
+    mensajes.textContent= ` ✅ Tenes acceso ${nombre}`
+} else {
+    mensajes.textContent= ` 📛 no Tenes acceso ${nombre}, tu edad es menor a 18`
+}
+console.log(nombre + edad)
+}
+
+form.addEventListener("submit", validacionDeFormulario)
